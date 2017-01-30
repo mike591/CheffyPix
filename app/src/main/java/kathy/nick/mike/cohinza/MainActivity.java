8d50d05a770d714ba5d01165897e71319abbc4a5
Michael <mikemach123@gmail.com>
2017-01-30 11:15:48 -0800

    Add buttons and linked button to activity

--- app/src/main/java/kathy/nick/mike/cohinza/MainActivity.java
+++ app/src/main/java/kathy/nick/mike/cohinza/MainActivity.java
@@ -2,12 +2,26 @@ package kathy.nick.mike.cohinza;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton = (Button) findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.app_name, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }

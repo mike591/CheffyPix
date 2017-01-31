e9c001ee2ccddd9d7ef4adc64f96adf5269878a4
Michael <mikemach123@gmail.com>
2017-01-31 11:33:00 -0800

    Fix white color

--- app/src/main/java/kathy/nick/mike/cohinza/MainActivity.java
+++ app/src/main/java/kathy/nick/mike/cohinza/MainActivity.java
@@ -1,27 +1,36 @@
 package kathy.nick.mike.cohinza;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mButton;
+    private static final String QUERY_KEY = "QUERY_KEY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mButton = (Button) findViewById(R.id.button);
+        mButton = (Button) findViewById(R.id.home_page_search_button);
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, R.string.app_name, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, RecipeDetailActivity.class);
+                EditText mEditText = (EditText) findViewById(R.id.home_page_search);
+                String message = mEditText.getText().toString();
+                intent.putExtra(QUERY_KEY, message);
+                startActivity(intent);
             }
         });
+
+
 
     }
 }

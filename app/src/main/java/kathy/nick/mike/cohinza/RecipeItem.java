c8803a65cb8f558b48dc8d23a9693210fec2171a
Nick Vizzutti <njvizzutti@gmail.com>
2017-01-31 10:53:24 -0800

    non

--- app/src/main/java/kathy/nick/mike/cohinza/RecipeItem.java
+++ app/src/main/java/kathy/nick/mike/cohinza/RecipeItem.java
@@ -14,6 +14,46 @@ public class RecipeItem {
     private int mSocialRank;
     private ArrayList mIngredients;
 
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public String getImageUrl() {
+        return mImageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        mImageUrl = imageUrl;
+    }
+
+    public int getRecipeId() {
+        return mRecipeId;
+    }
+
+    public void setRecipeId(int recipeId) {
+        mRecipeId = recipeId;
+    }
+
+    public int getSocialRank() {
+        return mSocialRank;
+    }
+
+    public void setSocialRank(int socialRank) {
+        mSocialRank = socialRank;
+    }
+
+    public ArrayList getIngredients() {
+        return mIngredients;
+    }
+
+    public void setIngredients(ArrayList ingredients) {
+        mIngredients = ingredients;
+    }
+
     public RecipeItem(String title, String imageUrl, int socialRank, int recipeId, ArrayList ingredients) {
         mTitle = title;
         mImageUrl = imageUrl;
